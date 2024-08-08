d05cda30b0bb2c1fdd8c6bdb906d753a8994ad32
Fedor <s02210519@gse.cs.msu.ru>
2024-08-08 17:21:31 +0300

    Initial Commit

---
+++ firstApp/firstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstAppApp.swift
+//  firstApp
+//
+//  Created by  Федор Попко on 08.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct firstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
